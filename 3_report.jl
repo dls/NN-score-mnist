3c0b918530fdd4578c9cf5ea52416cba6df59f00
David Salamon <dls@lithp.org>
2020-09-17 23:08:09 -0700

    adds set id to reporting, fixes ks related bug, adds plotting

--- 3_report.jl
+++ 3_report.jl
@@ -1,4 +1,9 @@
+using DataFrames
+using Gadfly
 using Serialization
+
+import Cairo, Fontconfig
+
 
 struct RunResult
   output :: Array{Float32, 1}
@@ -19,14 +24,14 @@ function percent_less_than(x, pos, neg)
   return pos_len / (pos_len + neg_len)
 end
 
-function confidence_plus_softmax(f, pos1, neg1)
-  println(f, "predicted category, actual category, true_positive, score, gt confidence, lt confidence")
+function confidence_plus_softmax(f, set_id, pos1, neg1)
+  println(f, "dataset, predicted category, actual category, true_positive, score, gt confidence, lt confidence")
   for cat=1:10
     for e=sort(vcat(pos1[cat], neg1[cat]), by = e -> e.score)
       gt = percent_greater_than(e.score, pos1[cat], neg1[cat])
       lt = percent_less_than(e.score, pos1[cat], neg1[cat])
       correct = e.max_output_i == e.correct_category ? 1 : 0
-      println(f, "$cat, $(e.correct_category + 1), $correct, $(e.score), $gt, $lt")
+      println(f, "$set_id, $cat, $(e.correct_category + 1), $correct, $(e.score), $gt, $lt")
     end
   end
 end
@@ -113,7 +118,7 @@ function main()
     m = length(test1_pos[i]) + length(test1_neg[i])
     n = length(test2_pos[i]) + length(test2_neg[i])
 
-    alpha = seek_alpha(ks_greater[i], m, n)
+    alpha = seek_alpha(ks_lesser[i], m, n)
 
     # println("sqrt(-log(a/2)/2) * sqrt(($n+$m) / ($n*$m)) = $(ks_lesser[i])    [a should be equal to $alpha")
 
@@ -123,15 +128,43 @@ function main()
 
 
   println()
-  println("Writing extra info to disk")
+  println("Writing score details to disk")
 
   f = open("3_scoring_extra_info.csv", "w")
-  println(f, "test set 1:")
-  confidence_plus_softmax(f, test1_pos, test1_neg)
+  confidence_plus_softmax(f, 1, test1_pos, test1_neg)
+  confidence_plus_softmax(f, 2, test2_pos, test2_neg)
+  close(f)
 
-  println(f, "")
-  println(f, "test set 2:")
-  confidence_plus_softmax(f, test2_pos, test2_neg)
+  println()
+  println("Writing plots")
+
+  plots = Array{Plot,1}()
+  function render_scatter_plot(set_name, category, pos, neg)
+    values = DataFrames.DataFrame(score = [], correct = [], confidence=[])
+    for e = pos
+      push!(values, Dict(:score => e.score,
+                         :correct => true,
+                         :confidence => percent_greater_than(e.score, pos, neg)))
+    end
+    for e = neg
+      push!(values, Dict(:score => e.score,
+                         :correct => false,
+                         :confidence => percent_greater_than(e.score, pos, neg)))
+    end
+
+    return plot(values, Guide.title("$set_name - Digit $(category-1)"),
+                x=:score, y=:confidence, color=:correct, Geom.point,
+                Scale.x_continuous(minvalue=0, maxvalue=10.0),
+                Scale.y_continuous(minvalue=0.85, maxvalue=1.0))
+  end
+
+  for cat=1:10
+    push!(plots, render_scatter_plot("Set 1", cat, test1_pos[cat], test1_neg[cat]))
+    push!(plots, render_scatter_plot("Set 2", cat, test2_pos[cat], test2_neg[cat]))
+  end
+
+  set_default_plot_size(6inch, 80inch)
+  vstack(plots) |> PDF("3_scatterplot.pdf")
 
   println("all done.")
 end
